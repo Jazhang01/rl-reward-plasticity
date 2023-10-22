Fix lyle plasticity calculation bug

jaxrl_m/jaxrl_m/evaluation.py
@@ -161,6 +161,6 @@ class EpisodeMonitor(gym.ActionWrapper):
 
         return observation, reward, term, trunc, info
 
-    def reset(self) -> np.ndarray:
+    def reset(self):
         self._reset_stats()
         return self.env.reset()

plasticity/plasticity/plasticity.py
@@ -43,7 +43,7 @@ def update_mse_lyle(
         # apply transformation to qs
         transformed_qs = qs.mean() + jnp.sin(1e5 * rand_qs)
 
-        loss = ((transformed_qs - rand_qs) ** 2).mean()
+        loss = ((qs - transformed_qs) ** 2).mean()
 
         info = {"loss": loss}
         return loss, info